f7ebed8a094b6602d153b8690041d7e61dfe3ca5
Aditya <aditya.novadianto@gmail.com>
2024-02-07 19:10:14 +0700

    create responsive in mobile version

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector('.navbar');
 const navbarContent = document.querySelector('.navbar-content');
+const navbarContentMobile = document.querySelector('.navbar-content-mobile');
+const hamburgerButton = document.querySelector('#hamburger');
 const inputName = document.getElementById('name');
 const inputEmail = document.getElementById('email');
 const buttonSumbit = document.getElementById('submit')
@@ -10,9 +12,11 @@ window.addEventListener('scroll', () => {
   if (window.scrollY > 150) {
     nav.classList.add('navbar-scroll');
     navbarContent.classList.add('navbar-content-scroll');
+    hamburgerButton.classList.add('hamburger-scroll');
   } else {
     nav.classList.remove('navbar-scroll');
     navbarContent.classList.remove('navbar-content-scroll');
+    hamburgerButton.classList.remove('hamburger-scroll');
   }
 });
 
@@ -50,7 +54,24 @@ buttonSumbit.addEventListener('click', (e) => {
   }
 
   // console.log(inputName.value, inputEmail.value)
+  
   if (inputName.value !== '' && inputEmail.value !== '') {
     alert('Terima Kasih Telah Mengisi Formulir Ini!')
+  }
+})
+
+hamburgerButton.addEventListener('click', () => {
+  navbarContentMobile.classList.toggle('show');
+})
+
+const isClickOutside = (e) => {
+  return !navbarContentMobile.contains(e.target) && !hamburgerButton.contains(e.target);
+}
+
+document.addEventListener('click', (e) => {
+  if (isClickOutside(e)) {
+    navbarContentMobile.classList.remove('show');
+  } else {
+    return
   }
 })
